bb0cadcb15cb692bbb95838d08c20ff3901a0a6b
Bosona19 <161635012+Bosona19@users.noreply.github.com>
2024-11-04 14:51:45 +0300

    simple clalculator

--- App.js
+++ App.js
@@ -1,20 +1,93 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+  const[input, setInput] = useState('')
+  const[result, setResult] = useState('')
+
+  const onButtonPress = (value) =>{
+    if(value === '='){
+      try{
+        setResult(eval(input))
+      } catch(error){
+        setResult('error')
+      }
+    } else if(value === 'c'){
+      setInput('');
+      setResult('')
+    }else {
+      setInput(input + value)
+    }
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-    </View>
+      <View style={styles.resultContainer}>
+        <Text style={styles.resultText}>
+          {result}
+        </Text>
+      </View>
+      <View style={styles.inputContainer}>
+        <TextInput 
+        style={styles.inputText}
+        value={input}
+        onChangeText={setInput}
+        keyboardType='numeric'
+          />
+      </View>
+      <View style={styles.buttonContainer}>
+            {['7','8','9','/','4','5','6','*','1','2','3','-','c','0','=','+'].map(
+              (item,index) =>(
+                <TouchableOpacity
+                key={index}
+                style={styles.button}
+                onPress={()=> onButtonPress(item)}
+                >
+                  <Text style={styles.buttontext}>{item}</Text>
+                </TouchableOpacity>
+              )
+            )}
+
+          </View>
+      </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex:1
   },
+  resultContainer: {
+    flex:2,
+    justifyContent:'center',
+    alignItems:'flex-end'
+  },
+  resultText: {
+    fontSize:40
+  },
+  inputContainer: {
+    flex:1,
+    justifyContent:'center',
+    alignItems:'flex-end'
+  },
+  inputText: {
+    fontSize:30
+  },
+  buttonContainer: {
+    flex:7,
+    flexDirection:'row',
+    flexWrap:'wrap'
+  },
+  button: {
+    width:"25%",
+    height:"20%",
+    justifyContent:'center',
+    alignItems:'center',
+    borderWidth:1,
+    borderColor:'#ccc'
+  },
+  buttontext: {
+    fontSize:24
+  }
 });
